48c71374cad9368c3300586d552e4c49f2df6dc8
unknown <cjsoccer17@knights.ucf.edu>
2021-03-14 23:54:52 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,11 +13,24 @@ public class PlayerScript : MonoBehaviour
 
     private int scoreValue = 0;
 
+    public Text lives;
+
+    private int livesValue = 3;
+
+    public Text conditionText;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        conditionText.text = "";
+        
+
     }
 
     // Update is called once per frame
@@ -26,9 +39,20 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+       
         if (Input.GetKey("escape"))
         {
             Application.Quit();
+        }
+          
+        if (livesValue == 0)
+        {
+            conditionText.text = "Game over you lose!";
+        }
+                  
+        if (scoreValue == 17)
+        {
+            conditionText.text = "Congrats! You Win! Thanks for playing! Game created by Chris Williams.";
         }
     }
 
@@ -37,10 +61,19 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
-
+        else if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (scoreValue == 10)
+        {
+            transform.position = new Vector3(85.0f, 6.00f);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -53,4 +86,5 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+    
 }
